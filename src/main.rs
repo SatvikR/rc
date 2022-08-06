0b4d0f2ad42fe89cd948a2b3164aace669562faf
Satvik Reddy <reddy.satvik@gmail.com>
2022-08-05 18:46:14 -0700

    timing compiler

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     env,
     io::{self, Write},
     process::{exit, Command},
+    time::Instant,
 };
 
 use lexer::load_src_file;
@@ -30,6 +31,10 @@ fn cmd_and_log(cmd: &str) {
     }
 }
 
+fn log(msg: &str) {
+    println!("[COMP] {}", msg);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -38,24 +43,39 @@ fn main() {
         exit(1);
     }
 
+    let full_compile_clock = Instant::now();
+
+    let read_clock = Instant::now();
     let program_path = &args[1];
     let src_str = load_src_file(program_path);
 
     let src_buf = src_str.as_bytes();
     let src_file = &SourceFile::new(program_path.to_string(), src_buf);
+    log(&format!("read source file in {:?}", read_clock.elapsed()));
 
+    let lex_clock = Instant::now();
     let mut lexer = Lexer::new(src_file);
     let tokens = lexer.lex();
+    log(&format!("lexed program in {:?}", lex_clock.elapsed()));
 
+    let parse_clock = Instant::now();
     let mut parser = Parser::new(tokens);
     let parsed_program = parser.parse();
+    log(&format!("parsed program in {:?}", parse_clock.elapsed()));
 
+    let asm_clock = Instant::now();
     let src_asm = format!("{}.asm", src_file.get_root());
     let src_obj = format!("{}.o", src_file.get_root());
     let src_exe = format!("{}", src_file.get_root());
 
     generate_x86_64(parsed_program, &src_asm).unwrap();
+    log(&format!("generated assembly in {:?}", asm_clock.elapsed()));
 
     cmd_and_log(&format!("nasm -felf64 {} -o {}", src_asm, src_obj));
     cmd_and_log(&format!("ld -o {} {}", src_exe, src_obj));
+
+    log(&format!(
+        "compiled program in {:?}",
+        full_compile_clock.elapsed()
+    ));
 }
